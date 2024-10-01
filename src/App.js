004ac871dc7076bb3bd31f9a4a23f68fdcbc130d
Payne Jones <paynejones01@gmail.com>
2024-10-01 15:08:07 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img className="profileimg" src="https://preview.redd.it/59v38gtva7dd1.jpeg?width=640&crop=smart&auto=webp&s=0d1f1d7b0c67fce26bb3d016cb47458cfcd5f2f8"/>
+      <div className="links">
+        <a href="https://github.com/paynejones"><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png"/></a>
+        <a href="https://www.linkedin.com/in/payne-jones/"><img src="https://upload.wikimedia.org/wikipedia/commons/0/06/Linkedin_circle_black-512.png"/></a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
