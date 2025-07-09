68788ee1d11d1de07f0df36c79420ebbd70b2396
Payne Jones <paynejones01@gmail.com>
2025-07-09 16:21:08 -0700

    Fix overscroll and replace email with resume

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
 import './Header.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faFile, faUser } from '@fortawesome/free-solid-svg-icons';
+import resume from "../static/Payne_Jones_Updated_Resume_July_2025.pdf";
 
 
 
@@ -10,8 +11,8 @@ export default function Header(){
         <header className='header'>
             <div className='links'>
                 <a className='link' href="https://github.com/paynejones" aria-label="My Github Account"><FontAwesomeIcon icon={faGithub} size="xl"/></a>
-                <a className='link' href="https://www.linkedin.com/in/payne-jones/" aria-label="My LinkedIn Account"><FontAwesomeIcon icon={faLinkedin} size="xl"/></a>
-                <a className='link' href="mailto:paynejones01@gmail.com" aria-label="My Email"><FontAwesomeIcon icon={faEnvelope} size="xl"/></a>
+                <a className='link' href="https://www.linkedin.com/in/payne-jones/" aria-label="My LinkedIn Account"><FontAwesomeIcon icon={faLinkedin} size="xl" transform="grow-1"/></a>
+                <a className='link' href={resume} download="Payne_Jones_Resume.pdf" aria-label="My Resume"><FontAwesomeIcon icon={faUser} size="xl" transform="shrink-7 down-1.5" mask={faFile}/></a>
             </div>
             <div className='center-name'>
                 Payne Jones
